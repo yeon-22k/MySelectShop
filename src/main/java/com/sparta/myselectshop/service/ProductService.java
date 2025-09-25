8b4920fdee8eb412b5d4dc22445cf7a5e66f36e5
yeon-22k <pink5465y@gmail.com>
2025-09-25 15:45:45 +0900

    상품 관련 API

---
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -0,0 +1,53 @@
+package com.sparta.myselectshop.service;
+
+import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
+import com.sparta.myselectshop.dto.ProductRequestDto;
+import com.sparta.myselectshop.dto.ProductResponseDto;
+import com.sparta.myselectshop.entity.Product;
+import com.sparta.myselectshop.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public static final int MIN_MY_PRICE = 100;
+
+    public ProductResponseDto createProduct(ProductRequestDto productRequestDto) {
+        Product product = productRepository.save(new Product(productRequestDto));
+        return new ProductResponseDto(product);
+    }
+
+    @Transactional
+    public ProductResponseDto updateProduct(Long id, ProductMypriceRequestDto productMypriceRequestDto) {
+        int myprice = productMypriceRequestDto.getMyprice();
+        if (myprice < MIN_MY_PRICE) {
+            throw new IllegalArgumentException("유효하지 않은 관심 가격입니다. 최소 " + MIN_MY_PRICE + "원 이상으로 설정해 주세요");
+        }
+
+        Product product = productRepository.findById(id).orElseThrow(() ->
+            new NullPointerException("해당 상품을 찾을 수 없습니다.")
+        );
+
+        product.update(productMypriceRequestDto);
+
+        return new ProductResponseDto(product);
+    }
+
+    public List<ProductResponseDto> getPRoducts() {
+        List<Product> productList = productRepository.findAll();
+        List<ProductResponseDto> responseDtos = new ArrayList<>();
+
+        for (Product product : productList) {
+            responseDtos.add(new ProductResponseDto(product));
+        }
+
+        return responseDtos;
+    }
+}
